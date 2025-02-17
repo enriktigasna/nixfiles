e0b332509dfaff78fdd0d211215410d895fa47fa
enriktigasna <42349218+enriktigasna@users.noreply.github.com>
2025-02-17 09:38:45 +0100

    Update home.nix

--- home.nix
+++ home.nix
@@ -8,39 +8,23 @@
 
   home.packages = with pkgs; [
     neovim
+    firefox
+    vscode-fhs
+    git
+    gitkraken
   ];
 
-  # basic configuration of git, please change to your own
   programs.git = {
     enable = true;
-    userName = "Ryan Yin";
-    userEmail = "xiaoyin_c@qq.com";
   };
 
-  # starship - an customizable prompt for any shell
   programs.starship = {
     enable = true;
-    # custom settings
     settings = {
       add_newline = false;
       aws.disabled = true;
       gcloud.disabled = true;
       line_break.disabled = true;
-    };
-  };
-
-  # alacritty - a cross-platform, GPU-accelerated terminal emulator
-  programs.alacritty = {
-    enable = true;
-    # custom settings
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 12;
-        draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
     };
   };
 
@@ -58,6 +42,9 @@
       urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
     };
   };
+
+  programs.zsh.enable = true;
+
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
 }
